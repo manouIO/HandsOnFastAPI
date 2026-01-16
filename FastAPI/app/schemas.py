022d8e77180053b947d5290b908ab689c242f648
manouIO <maryse@aims.ac.za>
2026-01-16 20:37:55 +0200

    modify: updated schemas file

---
+++ FastAPI/app/schemas.py
@@ -0,0 +1,37 @@
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+
+#title str, content str,  do not add anything else
+# using pydantic models for data validation
+class PostBase(BaseModel):
+    title: str
+    content: str
+    published: bool = True  #default value
+    #rating: Optional[int] = None  #optional field
+
+class PostCreate(PostBase):
+    pass
+
+#Let's create a schema for the response model
+class Post(PostBase):
+    id: int
+    created_at: datetime
+    
+    class Config:
+        orm_mode = True  #to work with ORM objects like SQLAlchemy
+
+
+#Schemas for User
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str 
+
+#Schema for User response model
+class User(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime
+
+    # class Config: #we want to work with ORM objects
+    #     orm_mode = True
+    model_config = {"from_attributes": True}

6d4e50d3219d90d1c535627da6f4733f483f8e12
manouIO <maryse@aims.ac.za>
2026-01-16 20:39:48 +0200

    modify:created user table

--- FastAPI/app/models.py
+++ FastAPI/app/models.py
@@ -11,3 +11,11 @@ class Post_alchemy(Base):
     content = Column(String, nullable=False)
     published = Column(Boolean, server_default='TRUE', nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()')) 
+
+class User_alchemy(Base):
+    __tablename__ = "users_alchemy"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    email = Column(String, nullable=False, unique=True) #ensure unique emails and prevent null values
+    password = Column(String, nullable=False) # no need for unique passwords
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))

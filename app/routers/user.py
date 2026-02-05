6ba932f306423d9d4be2a3fd3eb447bad26846e7
manouIO <maryse@aims.ac.za>
2026-02-05 15:18:46 +0200

    Update user router prefix and tags

--- app/routers/user.py
+++ app/routers/user.py
@@ -3,12 +3,15 @@ from .. import models, schemas, utils
 from sqlalchemy.orm import Session
 from ..database import get_db
 
-router = APIRouter()
+router = APIRouter(
+    prefix="/sqlalchemy/users",
+    tags=['Users']
+)
 
 
 
 # Create a user using SQLAlchemy
-@router.post("/sqlalchemy/users",status_code=status.HTTP_201_CREATED,response_model=schemas.User)
+@router.post("/",status_code=status.HTTP_201_CREATED,response_model=schemas.User)
 def create_user_sqlalchemy(user: schemas.UserCreate, db: Session = Depends(get_db)):
     print("==========================")
     hashed_password = utils.hash_password(user.password) #hash the password before storing
@@ -20,7 +23,7 @@ def create_user_sqlalchemy(user: schemas.UserCreate, db: Session = Depends(get_d
     return db_user
 
 # Get a user by id using SQLAlchemy
-@router.get("/sqlalchemy/users/{id}",response_model=schemas.User)
+@router.get("/{id}",response_model=schemas.User)
 def get_user_sqlalchemy(id:int, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.id == id).first()
     if not user:

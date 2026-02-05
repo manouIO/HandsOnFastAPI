69d3eb324a24846a6591f3dc2ae20ad702b90f8c
manouIO <maryse@aims.ac.za>
2026-02-05 10:14:22 +0200

    Delete Linux directory

--- Linux/Missing_semester/manou_001.sh
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/bash
-echo 'My name is Manou and I have two kids'
-echo My laptop is a beast. the current username is "$USERNAME",,the current shell is "$SHELL".
-echo I have always dreamed of printing my path to everyone. here it is: "$PATH"
